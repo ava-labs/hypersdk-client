3e234e72d44f3633817f5fa4756658c77689aaf6
Containerman17 <8990432+containerman17@users.noreply.github.com>
2024-09-19 07:28:53 +0000

    test update

--- src/lib/Marshaler.tx.test.ts
+++ src/lib/Marshaler.tx.test.ts
@@ -35,19 +35,47 @@ test('Empty transaction', () => {
   );
 })
 
+const minimalABI = {
+  "actions": [
+    {
+      "id": 0,
+      "name": "Transfer"
+    },
+  ],
+  "outputs": [],
+  "types": [
+    {
+      "name": "Transfer",
+      "fields": [
+        {
+          "name": "to",
+          "type": "Address"
+        },
+        {
+          "name": "value",
+          "type": "uint64"
+        },
+        {
+          "name": "memo",
+          "type": "Bytes"
+        }
+      ]
+    },
+  ]
+}
+
 test('Single action tx sign and marshal', async () => {
   const chainId = idStringToBigInt("2c7iUW3kCDwRA9ZFd5bjZZc8iDy68uAsFSBahjqSZGttiTDSNH");
-  const addrString = "001b057de320297c29ad0c1f589ea216869cf1938d88c9fbd70d6748323dbf2fa7";
+  const addrString = "0102030405060708090a0b0c0d0e0f101112131400000000000000000000000000";
 
-  const abiJSON = fs.readFileSync(`./src/testdata/abi.json`, 'utf8')
-  const marshaler = new Marshaler(JSON.parse(abiJSON) as VMABI)
+  const marshaler = new Marshaler(minimalABI)
 
   const actionData = {
-    actionName: "MockActionTransfer",
+    actionName: "Transfer",
     data: {
       to: addrString,
-      value: "123",
-      memo: Buffer.from("memo").toString('base64'),
+      value: "1000",
+      memo: Buffer.from("hi").toString('base64'),
     }
   }
 
@@ -60,23 +88,15 @@ test('Single action tx sign and marshal', async () => {
 
   const digest = marshaler.encodeTransaction(tx)
 
-  const expectedDigest = "0000018fcbcdeef0d36e467c73e2840140cc41b3d72f8a5a7446b2399c39b9c74d4cf077d250902400000002540be400" +
-    "01" + //how many actions
-    "00" + //action id
-    "1b057de320297c29ad0c1f589ea216869cf1938d88c9fbd70d6748323dbf2fa7" + //from
-    "00000000000000007b" + //value
-    "000000046d656d6f" //memo
-
+  const expectedDigest = "0000018fcbcdeef0d36e467c73e2840140cc41b3d72f8a5a7446b2399c39b9c74d4cf077d250902400000002540be40001000102030405060708090a0b0c0d0e0f10111213140000000000000000000000000000000000000003e8000000026869"
   expect(Buffer.from(digest).toString('hex')).toBe(expectedDigest);
-
-
 
   const privateKeyHex = "323b1d8f4eed5f0da9da93071b034f2dce9d2d22692c172f3cb252a64ddfafd01b057de320297c29ad0c1f589ea216869cf1938d88c9fbd70d6748323dbf2fa7";
   const privateKey = hexToBytes(privateKeyHex).slice(0, 32)
 
   const privateKeySigner = new PrivateKeySigner(privateKey);
 
-  const signedTxBytes = await privateKeySigner.signTx(tx, JSON.parse(abiJSON) as VMABI);
+  const signedTxBytes = await privateKeySigner.signTx(tx, minimalABI);
 
-  expect(Buffer.from(signedTxBytes).toString('hex')).toBe("0000018fcbcdeef0d36e467c73e2840140cc41b3d72f8a5a7446b2399c39b9c74d4cf077d250902400000002540be40001001b057de320297c29ad0c1f589ea216869cf1938d88c9fbd70d6748323dbf2fa700000000000000007b000000046d656d6f001b057de320297c29ad0c1f589ea216869cf1938d88c9fbd70d6748323dbf2fa72df1b5e3ea1dcf780b70e3c5f4f00ff3d28505ba26a83d7f60f5b691ec301f7b9ed128f5f5fc6289fcff736ba89b22e2fc15644d0355c778e014177b2a8f200c");
+  expect(Buffer.from(signedTxBytes).toString('hex')).toBe("0000018fcbcdeef0d36e467c73e2840140cc41b3d72f8a5a7446b2399c39b9c74d4cf077d250902400000002540be40001000102030405060708090a0b0c0d0e0f10111213140000000000000000000000000000000000000003e8000000026869001b057de320297c29ad0c1f589ea216869cf1938d88c9fbd70d6748323dbf2fa7b86baec5fe89f2bb585cb781f694a398107fe760577c750da3e9b381c5f5a3673c4a85c65a0db8d5ed03b4c4fd7f818d99270504e65c0ebf4d73884e0bfce60a");
 });
